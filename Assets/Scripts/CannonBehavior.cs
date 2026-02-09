dbd6215ff3e6ae9b71938cbbcd1bfd4a67b25d6f
Janelle N <142460244+janeln@users.noreply.github.com>
2026-02-09 14:24:41 -0700

    Added cannon aiming + small changed
    - Cannon can be aimed left and right with an adjustable aim speed
    - Cannon is tilted slightly forward to reflect perspective of the game (change this as needed)
    - Cannon shoots pre-existing ball object instead of instantiated prefab

--- Assets/Scripts/CannonBehavior.cs
+++ Assets/Scripts/CannonBehavior.cs
@@ -2,27 +2,51 @@ using UnityEngine;
 
 public class CannonBehavior : MonoBehaviour
 {
-    [SerializeField] private GameObject ballPrefab; // Player-controlled ball
+    [SerializeField] private GameObject ball; // Player-controlled ball
     [SerializeField] private float shotMagnitude = 15f; // Intensity of shot from cannon
-    GameObject ball;
-    Rigidbody rbBall;
-    bool ballShot = false;
-
+    [SerializeField] private float cannonRotationSpeed = 14f;
+    
+    private Rigidbody rbBall; // Physics object for ball
+    private bool spacePressed = false; // Key to trigger cannon
+    bool gameStarted = false; // True after cannon shoots the ball
+    
     void Start()
     {
         // Places ball directly above placeholder cannon shape
-        ball = Instantiate(ballPrefab, transform.position + new Vector3(0,1.5f,0), transform.rotation);
         rbBall = ball.GetComponent<Rigidbody>();
 
         // May not be needed after cannon model is added, as the ball will be able to fall inside the cannon before it's shot
         rbBall.useGravity = false;
     }
 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Aiming left"); // Shows in Console view
+            gameObject.transform.Rotate(Vector3.back * cannonRotationSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Aiming right");
+            gameObject.transform.Rotate(Vector3.forward * cannonRotationSpeed * Time.deltaTime, Space.Self);
+        }
+
+        // Key press to shoot cannon within FixedUpdate
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spacePressed = true;
+        }
+
+    }
+
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !ballShot)
+
+        if (spacePressed && !gameStarted)
         {
-            Debug.Log("Space pressed -- cannon triggered"); // Shows in Console view
+            Debug.Log("Space pressed -- cannon triggered");
             
             // Re-enables gravity -- delete when cannon model is added (explanation above)
             rbBall.useGravity = true;
@@ -31,7 +55,7 @@ public class CannonBehavior : MonoBehaviour
             rbBall.AddForce(transform.up * shotMagnitude, ForceMode.Impulse);
             
             // Disables ability for this script to add force to the ball after it's in the air
-            ballShot = true;
+            gameStarted = true;
             
         }
     }
